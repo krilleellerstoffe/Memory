b3e6383c227a7cea90208759e421c0f6f60d8188
Joel Eriksson Sinclair <joeles76@gmail.com>
2024-05-23 12:15:22 +0200

    Added In-App Localization

--- app/src/main/java/se/mau/al0038/memory/ui/SettingsScreen.kt
+++ app/src/main/java/se/mau/al0038/memory/ui/SettingsScreen.kt
@@ -1,15 +1,24 @@
 package se.mau.al0038.memory.ui
 
-import androidx.compose.foundation.ScrollState
+import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.os.LocaleListCompat
 import se.mau.al0038.memory.MemoryTopBar
 import se.mau.al0038.memory.R
 
@@ -30,18 +39,52 @@ fun SettingsScreen(
             )
         }
     ) { innerPadding ->
-        Column(
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            Text(text = stringResource(id = R.string.languageHeader))
-            Button(onClick = {
-            }) {
-                Text("Eng/Sv")
+        SettingsBody(modifier = Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun SettingsBody(modifier: Modifier = Modifier) {
+    val scrollState = rememberScrollState()
+    LaunchedEffect(scrollState) {
+        Log.d("SettingsLaunchedEffect", "Scroll to: ${scrollState.maxValue}")
+        scrollState.animateScrollTo(
+            scrollState.maxValue,
+            animationSpec = tween(durationMillis = 10000, easing = EaseIn)
+        )
+    }
+
+    Column(
+        modifier = modifier
+    ) {
+        Text(text = stringResource(id = R.string.languageHeader))
+        val localeOptions = mapOf(
+            R.string.en to "en",
+            R.string.sv to "sv"
+        ).mapKeys { stringResource(it.key) }
+
+        localeOptions.keys.forEach { locale ->
+            Button(
+                onClick = {
+                    AppCompatDelegate.setApplicationLocales(
+                        LocaleListCompat.forLanguageTags(localeOptions[locale])
+                    )
+                }
+            ) {
+                Text(text = locale)
             }
+        }
+        Card(modifier = Modifier.padding(10.dp)) {
             Text(
                 text = stringResource(id = R.string.about),
-                Modifier.verticalScroll(ScrollState(0))
+                Modifier.verticalScroll(state = scrollState)
             )
         }
     }
+}
+
+@Composable
+@Preview
+fun PreviewSettingsBody() {
+    SettingsBody()
 }
