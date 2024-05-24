23a97769bdac6eb4ce7cc9d544cc3544332093bb
Joel Eriksson Sinclair <joeles76@gmail.com>
2024-05-24 12:13:01 +0200

    Updated Settings Screen UI and About paragraphs.
    Signed-off-by: Joel Eriksson Sinclair <joeles76@gmail.com>

--- app/src/main/java/se/mau/al0038/memory/ui/SettingsScreen.kt
+++ app/src/main/java/se/mau/al0038/memory/ui/SettingsScreen.kt
@@ -4,9 +4,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.animation.core.EaseIn
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,22 +13,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -72,6 +62,8 @@ fun SettingsBody(modifier: Modifier = Modifier) {
         )
     }
 
+    Background()
+
     Column(
         verticalArrangement = Arrangement.Center,
         modifier = modifier
@@ -84,17 +76,25 @@ fun SettingsBody(modifier: Modifier = Modifier) {
         ) {
             LanguagePicker()
         }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            DarkModeSwitch()
+        }
         Spacer(modifier = Modifier.height(16.dp))
         Divider()
         Spacer(modifier = Modifier.height(16.dp))
-        DarkModeSwitch()
-        Spacer(modifier = Modifier.height(16.dp))
-        Divider()
-        Spacer(modifier = Modifier.height(16.dp))
-        Card(modifier = Modifier
-            .padding(10.dp)
-            .fillMaxWidth()
-            .weight(1f)) {
+        Card(
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.background.copy(
+                    alpha = 0.5f
+                )),
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+                .weight(1f)
+            ) {
             Text(
                 text = stringResource(id = R.string.about),
                 Modifier.verticalScroll(state = scrollState)
@@ -103,79 +103,41 @@ fun SettingsBody(modifier: Modifier = Modifier) {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LanguagePicker() {
     val localeOptions = mapOf(
+        R.string.system_default to "",
         R.string.en to "en",
         R.string.sv to "sv"
-    ).mapKeys { stringResource(it.key) }
+    )
 
-    var expanded by remember { mutableStateOf(false) }
-    Box {
-        TextField(
-            readOnly = true,
-            enabled = false,
-            value = stringResource(R.string.languageHeader),
-            onValueChange = { },
-            trailingIcon = {
-                ExposedDropdownMenuDefaults.TrailingIcon(
-                    expanded = expanded
-                )
-            },
-            modifier = Modifier.clickable {
-                expanded = !expanded
-            }
-        )
-        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
-            localeOptions.keys.forEach { locale ->
-                Log.d("LanguageList", locale)
-                DropdownMenuItem(
-                    text = { Text(text = locale) },
-                    onClick = {
-                        expanded = false
-                        AppCompatDelegate.setApplicationLocales(
-                            LocaleListCompat.forLanguageTags(localeOptions[locale])
-                        )
-                    }
-                )
-            }
-            DropdownMenuItem(text = { Text(text = stringResource(id = R.string.system_default)) },
-                onClick = {
-                    expanded = false
-                    AppCompatDelegate.setApplicationLocales(
-                        LocaleListCompat.forLanguageTags("")
-                    )
-                })
+    SettingsDropdown(
+        label = stringResource(id = R.string.languageHeader),
+        items = localeOptions.keys.toList(),
+        selectedItem = localeOptions.keys.first(),
+        onItemSelected = {
+            AppCompatDelegate.setApplicationLocales(
+                LocaleListCompat.forLanguageTags(localeOptions[it])
+            )
         }
-    }
+    )
 }
 
 @Composable
 fun DarkModeSwitch() {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Button(
-            onClick = {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            }, modifier = Modifier.padding(horizontal = 4.dp)) {
-            Text(text = stringResource(id = R.string.dark_mode))
+    val options = mapOf(
+        R.string.system_default to AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM,
+        R.string.light_mode to AppCompatDelegate.MODE_NIGHT_NO,
+        R.string.dark_mode to AppCompatDelegate.MODE_NIGHT_YES,
+    )
+
+    SettingsDropdown(label = stringResource(id = R.string.dark_mode_header),
+        items = options.keys.toList(),
+        selectedItem = options.keys.first(),
+        onItemSelected = {
+            options[it]?.let { it1 -> AppCompatDelegate.setDefaultNightMode(it1) }
         }
-        Button(
-            onClick = {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            }, modifier = Modifier.padding(horizontal = 4.dp)) {
-            Text(text = stringResource(id = R.string.light_mode))
-        }
-        Button(
-            onClick = {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-            }, modifier = Modifier.padding(horizontal = 4.dp)) {
-            Text(text = stringResource(id = R.string.system_default))
-        }
-    }
+    )
 }
 
 @Composable
